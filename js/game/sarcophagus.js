b2f2bd85bbb4c9d14611976a8d0736481cd51ffe
umu+ <aydinumut82@gmail.com>
2019-01-18 10:08:08 +0300

    character modules added

---
+++ js/game/sarcophagus.js
@@ -0,0 +1,31 @@
+function Sarcophagus() {
+    this.dim = [128, 128];
+    this.img = new Image();
+    this.img.src = '/img/game/sarcophagus.png';
+    this.img.onload = this.__render.bind(this);
+}
+
+Sarcophagus.prototype.setClickHandler = function (fn) {
+    this.clickHandler = fn;
+}
+
+Sarcophagus.prototype.onReady = function (fn) {
+    this.readyHandler = fn;
+}
+
+Sarcophagus.prototype.__render = function () {
+    const self = this;
+    this.el = $('<div />').css('position', 'absolute')
+        .attr('id', 'sa_' + Math.random().toString().replace('0.', '').trim())
+        .css('top', (window.innerHeight - this.dim[1] * 2) * Math.random() + 'px')
+        .css('left', (window.innerWidth - this.dim[0] * 2) * Math.random() + 'px')
+        .css('width', this.dim[0] + 'px')
+        .css('height', this.dim[1] + 'px')
+        .css('background', 'url(' + self.img.src + ')')
+        .css('z-index', '999999990')
+        .addClass('building');
+    this.el.click(function () {
+        if (self.clickHandler) self.clickHandler.bind(this)();
+    });
+    if (self.readyHandler) self.readyHandler();
+}
